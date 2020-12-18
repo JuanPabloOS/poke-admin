2b773e1e6206d8869b2ab4379ab625f20974ad0d
Juan Olvera <jp.oes@hotmail.com>
2020-12-18 15:51:52 -0600

    add dummy data

---
+++ src/data/items.js
@@ -0,0 +1,182 @@
+export const ITEMS = [
+  {
+    id: 1,
+    name: 'master-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/master-ball.png',
+  },
+  {
+    id: 2,
+    name: 'ultra-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/ultra-ball.png',
+  },
+  {
+    id: 3,
+    name: 'great-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/great-ball.png',
+  },
+  {
+    id: 4,
+    name: 'poke-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png',
+  },
+  {
+    id: 5,
+    name: 'safari-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/safari-ball.png',
+  },
+  {
+    id: 6,
+    name: 'net-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/net-ball.png',
+  },
+  {
+    id: 7,
+    name: 'dive-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/dive-ball.png',
+  },
+  {
+    id: 8,
+    name: 'nest-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/nest-ball.png',
+  },
+  {
+    id: 9,
+    name: 'repeat-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/repeat-ball.png',
+  },
+  {
+    id: 10,
+    name: 'timer-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/timer-ball.png',
+  },
+  {
+    id: 11,
+    name: 'luxury-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/luxury-ball.png',
+  },
+  {
+    id: 12,
+    name: 'premier-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/premier-ball.png',
+  },
+  {
+    id: 13,
+    name: 'dusk-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/dusk-ball.png',
+  },
+  {
+    id: 14,
+    name: 'heal-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/heal-ball.png',
+  },
+  {
+    id: 15,
+    name: 'quick-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/quick-ball.png',
+  },
+  {
+    id: 16,
+    name: 'cherish-ball',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/cherish-ball.png',
+  },
+  {
+    id: 17,
+    name: 'potion',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/potion.png',
+  },
+  {
+    id: 18,
+    name: 'antidote',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/antidote.png',
+  },
+  {
+    id: 19,
+    name: 'burn-heal',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/burn-heal.png',
+  },
+  {
+    id: 20,
+    name: 'ice-heal',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/ice-heal.png',
+  },
+  {
+    id: 21,
+    name: 'awakening',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/awakening.png',
+  },
+  {
+    id: 22,
+    name: 'paralyze-heal',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/paralyze-heal.png',
+  },
+  {
+    id: 23,
+    name: 'full-restore',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/full-restore.png',
+  },
+  {
+    id: 24,
+    name: 'max-potion',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/max-potion.png',
+  },
+  {
+    id: 25,
+    name: 'hyper-potion',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/hyper-potion.png',
+  },
+  {
+    id: 26,
+    name: 'super-potion',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/super-potion.png',
+  },
+  {
+    id: 27,
+    name: 'full-heal',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/full-heal.png',
+  },
+  {
+    id: 28,
+    name: 'revive',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/revive.png',
+  },
+  {
+    id: 29,
+    name: 'max-revive',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/max-revive.png',
+  },
+  {
+    id: 30,
+    name: 'fresh-water',
+    imgUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/fresh-water.png',
+  },
+];
