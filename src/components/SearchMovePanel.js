d4d48f7fa5141d0960bf1ecc88ff8f176c56bfb1
Juan Olvera <jp.oes@hotmail.com>
2021-01-04 15:36:56 -0600

    feat: add moves filter

--- src/components/SearchMovePanel.js
+++ src/components/SearchMovePanel.js
@@ -1,26 +1,64 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import SideContainer from './SideContainer';
 import MoveCard from './MoveCard';
 import { MovesContext } from '../context/MovesProvider';
+import StyledSearchInput from '../components/SearchInput';
 import styled from 'styled-components';
 
-const ItemsContainer = styled.div`
-  height: 100%;
+const TopSide = styled.div`
+  height: 150px;
+  box-sizing: border-box;
+  padding: 1rem;
+  border-bottom: 1px solid ${(props) => props.theme.borderColor};
+  background-color: ${(props) => props.theme.primaryBg};
+`;
+
+const MiddleSide = styled.div`
+  height: calc(100% - 150px);
   overflow-y: scroll;
+  background-color: ${(props) => props.theme.primaryBg};
 `;
 const SearchMovePanel = () => {
   const { moves } = useContext(MovesContext);
-  return (
-    <SideContainer>
-      <ItemsContainer>
-        {moves.map((move, index) => (
+  const [searchText, setSearchText] = useState('');
+  function renderMovesList() {
+    if (searchText !== '') {
+      return moves
+        .filter((i) => i.name.startsWith(searchText))
+        .map((move, index) => (
           <MoveCard
             key={`move-${move.name}-${index}`}
             id={move.id}
             name={move.name}
           />
-        ))}
-      </ItemsContainer>
+        ));
+    }
+    return moves.map((move, index) => (
+      <MoveCard
+        key={`move-${move.name}-${index}`}
+        id={move.id}
+        name={move.name}
+      />
+    ));
+  }
+  return (
+    <SideContainer>
+      <TopSide>
+        <div className="py-2 d-flex flex-column">
+          <p className="text-primary st-8">
+            Welcome to the Pokémon administrative panel
+          </p>
+          <StyledSearchInput
+            type="text"
+            placeholder="Search"
+            className="customSearchInput"
+            data-testid="search-input"
+            value={searchText}
+            onChange={({ target }) => setSearchText(target.value)}
+          />
+        </div>
+      </TopSide>
+      <MiddleSide>{renderMovesList()}</MiddleSide>
     </SideContainer>
   );
 };
